5a20564a11e15855e30d085b0a9455ff0b94cc32
wannabil <wannabil.xyz@gmail.com>
2025-04-17 11:49:10 +0800

    moving the player using WASD button

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
+
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	
+	clock = pygame.time.Clock()
+	dt = 0
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+	player = Player(x, y)
+
 	while True:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         		    return
+
+		player.update(dt)
+
 		screen.fill((0,0,0))
-	pygame.display.flip()
+		player.draw(screen)
+		pygame.display.flip()
+
+		dt = clock.tick(60) / 1000 # limit FPS to 60 and get delta time from ms to seconds
 
 if __name__ == "__main__":
 	main()
